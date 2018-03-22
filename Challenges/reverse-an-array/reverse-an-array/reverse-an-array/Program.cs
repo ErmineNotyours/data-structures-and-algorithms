e89c8d041f2c515562d79844b949c6e7ef3b8322
ErmineNotyours <arthurallen@centurylink.net>
2018-03-22 11:06:22 -0700

    New gitting

---
+++ Challenges/reverse-an-array/reverse-an-array/reverse-an-array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace reverse_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseString();
+        }
+
+        static void ReverseString()
+        {
+            int[] array = { 7, 6, 5, 4, 3, 2, 1 }; // Data set
+            int[] newArray = new int[array.Length];
+            int arrayLength = array.Length;
+
+            for (int i = 0; i < (arrayLength); i++)
+            {
+
+                newArray[arrayLength - i - 1] = array[i]; // Populate newArray with data in the reverse order from array
+
+            }
+
+            Console.WriteLine("Old array: ");
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("New array: ");
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+            Console.Read();
+        }
+
+    }
+
+
+}
