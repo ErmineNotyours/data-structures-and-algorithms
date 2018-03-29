2eee342cc5383f621f83c4e8ccb322428f55c547
ErmineNotyours <arthurallen@centurylink.net>
2018-03-29 12:29:42 -0700

    Functionality added: adds a node to the end of a linked list.  Now need to add a value to the start of a linked list.

--- Data_Structures/LinkedList/LinkedList/Program.cs
+++ Data_Structures/LinkedList/LinkedList/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using LinkedList;
+
 
 namespace LinkedList
 {
@@ -8,18 +10,32 @@ namespace LinkedList
         {
             Console.WriteLine("Hello World!");
 
-            Node node0 = new Node();
             Node node1 = new Node();
             Node node2 = new Node();
             Node node3 = new Node();
             Node node4 = new Node();
 
-            node0.Next = node1;
             node1.Next = node2;
             node2.Next = node3;
             node3.Next = node4;
             node4.Next = null;
 
+            node1.Value = 2;
+            //node2.Value = 3;
+            node3.Value = 4;
+            node4.Value = 5;
+
+            SinglyLinked sll = new SinglyLinked(node1); // If this is set later than node1, list starts there
+
+            sll.PrintAllNodes(); // traverses the node, prints them all out
+
+            sll.Find(3); // Traverses the node, prints out the parameter if found
+
+            sll.Add(11); // Adds value to the start of the node.
+
+            Console.WriteLine("Here's the list again");
+            sll.PrintAllNodes(); // prints the list again to show it worked.
+            Console.ReadLine();
         }
 
 
