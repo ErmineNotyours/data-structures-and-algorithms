f93d9afc01168c2d0baa7f4b5116f129f21272dc
ErmineNotyours <arthurallen@centurylink.net>
2018-04-25 21:21:23 -0700

    Print Level Order complete

---
+++ Challenges/PrintLevelOrder/PrintLevelOrder/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintLevelOrder
+{
+    public class Node
+    {
+        public Node()
+        {
+            Children = new List<Node>();
+        }
+        public string Value { get; set; }
+        public List<Node> Children { get; set; }
+    }
+}
---
+++ Challenges/PrintLevelOrder/PrintLevelOrderTest/UnitTest1.cs
@@ -0,0 +1,108 @@
+using System;
+using PrintLevelOrder;
+using Xunit;
+
+namespace PrintLevelOrderTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TreeTest1()
+        {
+            // Arrange
+            Node node = new Node();
+
+            // Declare and populate the nodes
+            Node nodeA = new Node() { Value = "A" };
+            Node nodeB = new Node() { Value = "B" };
+            Node nodeC = new Node() { Value = "C" };
+            Node nodeD = new Node() { Value = "D" };
+            Node nodeE = new Node() { Value = "E" };
+            Node nodeF = new Node() { Value = "F" };
+            Node nodeG = new Node() { Value = "G" };
+            Node nodeH = new Node() { Value = "H" };
+            Node nodeI = new Node() { Value = "I" };
+            Node nodeJ = new Node() { Value = "J" };
+            Node nodeK = new Node() { Value = "K" };
+            Node nodeL = new Node() { Value = "L" };
+            Node nodeM = new Node() { Value = "M" };
+            Node nodeN = new Node() { Value = "N" };
+            Node nodeO = new Node() { Value = "O" };
+            Node nodeP = new Node() { Value = "P" };
+            Node nodeQ = new Node() { Value = "Q" };
+
+
+            // Instantiate the tree
+            MyTree tree = new MyTree(nodeA);
+
+            // Link the tree.
+            nodeA.Children.Add(nodeB);
+            nodeA.Children.Add(nodeC);
+            nodeA.Children.Add(nodeD);
+            nodeA.Children.Add(nodeE);
+            nodeA.Children.Add(nodeF);
+            nodeA.Children.Add(nodeG);
+
+
+            nodeB.Children.Add(nodeH);
+            nodeB.Children.Add(nodeI);
+            nodeB.Children.Add(nodeJ);
+
+
+            nodeE.Children.Add(nodeK);
+            nodeE.Children.Add(nodeL);
+
+            nodeG.Children.Add(nodeM);
+
+            nodeH.Children.Add(nodeN);
+            nodeH.Children.Add(nodeO);
+
+            nodeK.Children.Add(nodeP);
+
+            nodeL.Children.Add(nodeQ);
+
+            // Assert
+            Assert.Equal("A**NEWLINE**BCDEFG**NEWLINE**HIJKLM**NEWLINE**NOPQ", Program.PrintLevelOrder(nodeA));
+
+        }
+
+        [Fact]
+        public void TreeTest2()
+        {
+            // Arrange
+            Node node = new Node();
+
+            // Declare and populate the nodes
+            Node nodeA = new Node() { Value = "A" };
+            Node nodeB = new Node() { Value = "B" };
+            Node nodeC = new Node() { Value = "C" };
+
+            // Instantiate the tree
+            MyTree tree = new MyTree(nodeA);
+
+
+            // Link the tree
+            nodeA.Children.Add(nodeB);
+            nodeA.Children.Add(nodeC);
+
+            // Assert
+            Assert.Equal("A**NEWLINE**BC", Program.PrintLevelOrder(nodeA));
+        }
+
+        [Fact]
+        public void TreeTest3()
+        {
+            // Arrange
+            Node node = new Node();
+
+            // Declare and populate the node
+            Node nodeA = new Node() { Value = "A" };
+
+            // Instantiate the tree
+            MyTree tree = new MyTree(nodeA);
+
+            // Assert
+            Assert.Equal("A", Program.PrintLevelOrder(nodeA));
+        }
+    }
+}
