bd1aae32f1b3d89777c6811089f0fdf53ce899a2
ErmineNotyours <arthurallen@centurylink.net>
2018-04-26 13:59:07 -0700

    Stuck on recursively referencing a tree with nodes of a collection

---
+++ Challenges/FindMatches/FindMatches/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindMatches
+{
+    class Program
+    {
+        public static Node Children { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            Node node = new Node();
+
+            // Declare and populate the nodes
+            Node nodeA = new Node() { Value = "A" };
+            Node nodeB = new Node() { Value = "B" };
+            Node nodeC = new Node() { Value = "C" };
+            Node nodeD = new Node() { Value = "D" };
+            Node nodeE = new Node() { Value = "B" };
+            Node nodeF = new Node() { Value = "F" };
+            Node nodeG = new Node() { Value = "G" };
+            Node nodeH = new Node() { Value = "H" };
+            Node nodeI = new Node() { Value = "I" };
+            Node nodeJ = new Node() { Value = "J" };
+            Node nodeK = new Node() { Value = "B" };
+            Node nodeL = new Node() { Value = "L" };
+            
+
+
+            // Instantiate the tree
+            MyTree tree = new MyTree(nodeA);
+
+            // Link the tree.
+            nodeA.Children.Add(nodeB);
+            nodeA.Children.Add(nodeC);
+            nodeA.Children.Add(nodeD);
+            nodeA.Children.Add(nodeE);
+            nodeA.Children.Add(nodeF);
+            nodeA.Children.Add(nodeG);
+
+
+            nodeB.Children.Add(nodeH);
+            nodeB.Children.Add(nodeI);
+            nodeB.Children.Add(nodeJ);
+
+
+            nodeE.Children.Add(nodeK);
+            nodeE.Children.Add(nodeL);
+
+            string target = "B";
+
+            List<Node> collection = new List<Node>();
+
+            FindMatches(nodeA, target, collection);
+            foreach (var c in collection)
+                Console.Write(c.Value + ", ");
+
+            Console.ReadLine();
+
+
+        }
+
+        public static string FindMatches(Node node, string target, List<Node> collection)
+        {
+            string progress = "";
+            Traverse(node, target, collection);
+
+            return progress;
+        }
+
+        public static List<Node> Traverse(Node node, string target, List<Node> collection)
+        {
+            if (node.Value == target)
+            {
+                Console.WriteLine($"Adding: {node.Value}");
+                collection.Add(node);
+            }
+
+            foreach (var x in node.Children)
+            {
+                // Stack overflow.
+                if (node.Children != null)
+                {
+                    Console.WriteLine($"About to call Traverse, node: {node.Value}"); // Only ever prints "A".
+                    collection = Traverse(node.Children.x, target, collection);
+                }
+            }
+
+            return collection;
+        }
+    }
+}
---
+++ Challenges/FindMatches/FindMatches/Tree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMatches
+{
+    public class MyTree
+    {
+        public Node Root { get; set; }
+
+        /// <summary>
+        /// Sets the memory location for Root
+        /// </summary>
+        /// <param name="node">A node for a tree</param>
+        public MyTree(Node node)
+        {
+            Root = node;
+        }
+
+        /// <summary>
+        /// Performs a breadth-first traversal of a k-ary tree.
+        /// Start by enqueueing the root of the tree to a queue.
+        /// Now you can loop through, dequeueing the node, then pushing the children of the dequeued nodes onto the queue.  Once the queue is empty, the traversal is over.
+        /// </summary>
+        /// <param name="node">The incoming tree root</param>
+        public string BreadthFirst(Node node)
+        {
+            string progress = ""; // String collection of items traversed so far.  Used in testing.
+            var q = new Queue<Node>(); // Instantiate a built-in Queue.
+            q.Enqueue(node); // Enqueue the root of the tree to seed the process.
+
+            Node SampleNode = new Node(); // Node to dequeue and use to enqueue its children.
+
+            //While the queue is not empty...
+            while (q.Count > 0)
+            {
+                //Dequeue a node from the queue
+                SampleNode = q.Dequeue();
+
+                //Print the node's value
+                Console.Write($"{SampleNode.Value}, ");
+                progress += SampleNode.Value;
+
+                //Enqueue the child nodes, if they have any
+                //Use a foreach loop
+                foreach (var x in SampleNode.Children)
+                    q.Enqueue(x);
+            }
+
+            return progress;
+        }
+
+        public string AddNode(Node parent, string value)
+        {
+            // Test to see if tree already exists?
+
+            // Create new node
+            Node node = new Node() { Value = value };
+
+            // Link the node
+            parent.Children.Add(node);
+
+            return (node.Value);
+        }
+
+    }
+
+}
